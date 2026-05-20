055fba70ccbd9873b0c417f68408fa6ada5613d6
Tyro Beatty <judahglobal96@gmail.com>
2026-05-20 12:57:35 -0400

    Remove build artifacts and sql from repo

--- dist/routes/discovery.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const eventDiscovery_controller_1 = require("../controllers/eventDiscovery.controller");
-const router = (0, express_1.Router)();
-router.get("/events", eventDiscovery_controller_1.getAllDiscoveredEvents);
-exports.default = router;
--- dist/routes/orgEventSubmission.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const orgEventSubmission_controller_1 = require("../controllers/orgEventSubmission.controller");
-const router = (0, express_1.Router)();
-router.post("/submit-for-review", orgEventSubmission_controller_1.submitEventForReview);
-exports.default = router;
